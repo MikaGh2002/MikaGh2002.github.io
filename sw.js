b9befd8de86c34814e65b20b1aca63fd6c09771c
Michael <39475796+MikaGh2002@users.noreply.github.com>
2023-03-31 08:39:17 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,15 +9,15 @@ self.addEventListener('install', function(event) {
           '/blog.html',
           '/contact.html',
           '/images/logo.png',
-//        '/images/example-work01.jpg',
-//        '/images/example-work07.jpg',
-//        '/images/example-work02.jpg',
-//        '/images/example-work03.jpg',
-//        '/images/example-work04.jpg',
-//        '/images/example-work05.jpg',
-//        '/images/example-work06.jpg',
-//        '/images/example-work08.jpg',
-//        '/images/example-work09.jpg'
+          '/images/example-work01.jpg',
+          '/images/example-work07.jpg',
+          '/images/example-work02.jpg',
+          '/images/example-work03.jpg',
+          '/images/example-work04.jpg',
+          '/images/example-work05.jpg',
+          '/images/example-work06.jpg',
+          '/images/example-work08.jpg',
+          '/images/example-work09.jpg'
         ])
       })
   );
