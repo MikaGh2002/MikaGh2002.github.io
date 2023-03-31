cf2df5a1d411e1a536296a58faa5b3ee1140d716
Michael <39475796+MikaGh2002@users.noreply.github.com>
2023-03-31 07:49:47 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,23 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('first-app')
+    caches.open('MDL-cache')
       .then(function(cache) {
         cache.addAll([
           '/',
           '/index.html',
-          '/src/css/app.css',
-          '/src/js/app.js'
+          'about.html',
+          'blog.html',
+          'contact.html',
+          '/images/example-work01.jpg',
+          '/images/example-work07.jpg',
+          '/images/example-work02.jpg',
+          '/images/example-work04.jpg',
+          '/images/example-work05.jpg',
+          '/images/example-work03.jpg',
+          '/images/example-work06.jpg',
+          '/images/example-work08.jpg',
+          '/images/example-work09.jpg',
+          '/images/logo.png'
         ])
       })
   );
